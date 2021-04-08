0c6aa91b0f3d0ead0188d8ef22745024bd290c9c
Akhil Eppa <akhil.e2k@gmail.com>
2021-04-08 20:00:47 +0530

    Product Management

---
+++ Product_Management.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  8 15:46:35 2021
+
+@author: Akhil
+"""
+import pandas as pd
+import csv
+from csv import writer
+class Admin:
+    def __init__(self, username, password):
+        self.username=username
+        self.password=password
+        
+    def display_products(self):
+        with open('products.csv') as file:
+            csv_reader=csv.reader(file, delimiter=',')
+            product_count=0
+            for row in csv_reader:
+                if product_count!=0:
+                    print(f'Serial number {product_count}')
+                    print(f'Product ID -> {row[0]}')
+                    print(f'Product Name -> {row[1]}')
+                    print(f'Current Product Quantity -> {row[2]}')
+                    print(f'Product Price -> {row[3]}')
+                    print(f'Product Seller -> {row[4]}\n')
+                product_count+=1
+            self.product_count=product_count-1#To exclude header fields
+            file.close()
+            
+    def add_product(self, name, qty, price, seller):
+        self.product_count+=1
+        content=[self.product_count, name, qty, price, seller]
+        with open('products.csv','a',newline='') as file:
+            writer_object=writer(file)
+            writer_object.writerow(content)
+            file.close()
+            
+    def delete_product(self, num):
+        if num<=0 or num>self.product_count:
+            print("Wrong serial number entered!")
+        else:
+            print("Successfully deleted product from Catalogue. ")
+def main():
+    print("test")
+    df=pd.read_csv("admins.csv")
+    flag=1#flag is use to keep repeating till login is successful
+    while (flag):
+        print("------Admin Portal---------")
+        username=input("Enter username: ")
+        password=input("Enter password: ")
+        #First filter by given username
+        #Create subset of data with the given username
+        mask=df['username'].values==username
+        df_new=df[mask]
+        #In the subset of data check if password matches
+        mask=df_new['password'].values==password
+        df_new=df_new[mask]
+        if len(df_new)!=0:
+            print("Admin Login Successful!\n")
+            flag=0
+            #Clear Terminal
+            print(chr(27) + "[2J")
+        elif username=="exit":
+            break
+        else:
+            print("Incorrect Credentials. Try Again!")
+        
+    if flag==0:
+        login=1
+    else:
+        login=0
+    if login:
+        Ad=Admin(username,password)
+        Ad.display_products()
+        while(login):
+            print("\n-------------------------\n1. View Products\n2. Add Product\n3. Delete Product \n4. Modify Product\n5. Exit")
+            choice=int(input("Enter your choice: "))
+        
+            if choice==1:
+                Ad.display_products()
+            elif choice==2:
+                print(chr(27) + "[2J")
+                print("\n----Add Product-----")
+                name=input("Enter product name: ")
+                try:
+                    qty=int(input("Enter product quantity: "))
+                except:
+                    print("Incorrect format")
+                    continue
+                try:
+                    price=int(input("Enter product price: "))
+                except:
+                    print("Incorrect format")
+                    continue
+                seller=input("Enter seller name: ")
+                Ad.add_product(name, qty, price, seller)
+                print("Product Added successfully!\n")
+            elif choice==3:
+                Ad.display_products()
+                try:
+                    x=int(input("Enter serial number of product to be deleted: "))
+                    Ad.delete_product(x)
+                except:
+                    print(chr(27) + "[2J")
+                    print("Invalid Format!")
+            elif choice==4:
+                continue
+            elif choice==5:
+                login=0
+            else:
+                print(chr(27) + "[2J")
+                print("Incorrect Choice")
+        
+    #print(df[df["username"]=="aa" and df["password"]=="aa"])
+
+if __name__=="__main__":
+    main()
